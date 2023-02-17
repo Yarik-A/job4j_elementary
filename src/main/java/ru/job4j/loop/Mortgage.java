0a04867d7c45d256ed1d3935c125ab673c467a3e
Yaroslav A <yaroslaav@yandex.ru>
2023-02-17 13:33:19 +0300

    5.7. Ипотека [#156315 #392571]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -5,7 +5,7 @@ public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount = amount + amount * percent / 100 - salary;
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
